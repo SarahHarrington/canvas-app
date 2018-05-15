067e84803aff2d01cbe2bba94e7a6024c1796c4d
Sarah Harrington <sarah.soberg@gmail.com>
2018-05-15 15:48:03 -0500

    commiting before adding socket.io

--- server/server.js
+++ server/server.js
@@ -4,6 +4,10 @@ const port = 5000;
 
 app.use(express.static('server/public'));
 
+app.get('/', function(req, res) {
+  res.sendFile('server/public' + '/index.html');
+})
+
 app.listen(port, function() {
   console.log('listening on port', port)
 })
